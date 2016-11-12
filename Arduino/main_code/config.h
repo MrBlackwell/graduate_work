f35bcc9ac68a5aa8c108cfd2ad46fa70681e4b00
FrankyInDaHouse <vimicher@yandex.ru>
2016-11-12 10:26:18 +0300

    1.0

---
+++ Arduino/main_code/config.h
@@ -0,0 +1,13 @@
+struct configuration // структура описывающая количество подключенных датчиков различных типов
+{
+  int number_of_motion_sensors; // кол-во датчиков движения
+  int number_of_fume_sensors; // кол-во датчиков дыма/газа
+  int number_of_temperature_sensors; // кол-во датчиков температуры/влажности
+  int number_of_gerkon_sensors; // кол-во датчиков открывания дверей
+  int number_of_vibration_sensors; // кол-во датчиков вибрации
+  int number_of_water_sensors; // кол-во датчиков воды
+  int number_of_cams; // кол-во камер 
+};
+
+
+
---
+++ Arduino/main_code/main_code.ino
@@ -0,0 +1,18 @@
+#include "config.h"
+#include "data.h"
+configuration full_armed;
+
+void setup() {
+  full_armed.number_of_motion_sensors = 5;
+  
+  
+
+}
+
+void loop() {
+
+  
+  
+  
+
+}
