e82b63da30a98848d08bd03b346ac9cc83157930
FrankyInDaHouse <vimicher@yandex.ru>
2016-11-25 23:57:46 +0300

    v1.0.1

--- Arduino/main_code/config.h
+++ Arduino/main_code/config.h
@@ -1,13 +1,15 @@
-struct configuration // структура описывающая количество подключенных датчиков различных типов
+#define UNO -1
+#define MEGA -2
+
+class configuration // класс описывающий количество подключенных датчиков различных типов
 {
+  public:
   int number_of_motion_sensors; // кол-во датчиков движения
   int number_of_fume_sensors; // кол-во датчиков дыма/газа
   int number_of_temperature_sensors; // кол-во датчиков температуры/влажности
   int number_of_gerkon_sensors; // кол-во датчиков открывания дверей
   int number_of_vibration_sensors; // кол-во датчиков вибрации
   int number_of_water_sensors; // кол-во датчиков воды
-  int number_of_cams; // кол-во камер 
+  int number_of_cams; // кол-во камер
+  int board; // что за плату используем
 };
-
-
-
--- Arduino/main_code/main_code.ino
+++ Arduino/main_code/main_code.ino
@@ -1,18 +1,88 @@
 #include "config.h"
-#include "data.h"
-configuration full_armed;
+#include <dht11.h>      // Добавляем библиотеку DHT11(температуры)
 
-void setup() {
-  full_armed.number_of_motion_sensors = 5;
+  //прототипы функций
+  void download_config();
+
+
+  //объявление класса current_config типа configuration
+  configuration current_config;
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~МАССИВ ДАННЫХ С ДАТЧИКОВ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    dht11 DHT_array[1]; 
+   
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~МАССИВ ДАННЫХ С ДАТЧИКОВ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+void setup() 
+{
+  Serial.begin(9600);   // Скорость работы serial порта
+  
+  download_config(); // сначала скачиваем откуда-то конфигурацию устройства.(тут узнаем что будет подключено и сколько)
+  current_config.board = UNO; // используемая плата
+  current_config.number_of_temperature_sensors = 1;  // запишем в конфиг число датчиков температуры(потом это надо будет убрать) ПРОБЛЕМА!!!
+  
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~УСТАНОВИМ РАСПИНОВКУ ПОД ДАТЧИКИ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(current_config.board == UNO)
+  {
+        // распиновка под уну
+        #define DHT11_zero 2     // Нулевой датчик DHT11 подключен к цифровому пину номер 2
+        pinMode(DHT11_zero, INPUT);
+  }
+  else
+  {
+        // распиновка под мегу
+        
+    
+    
+  }
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~УСТАНОВИМ РАСПИНОВКУ ПОД ДАТЧИКИ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+   
+  
+  
+}
+
+void loop() 
+{
+      delay(300);
+      int chk = DHT_array[0].read(DHT11_zero);    // Чтение данных
+      /*switch (chk)
+      {
+      case DHTLIB_OK:  
+          break;
+      case DHTLIB_ERROR_CHECKSUM:
+          Serial.println("Checksum error, \t");
+          break;
+      case DHTLIB_ERROR_TIMEOUT:
+          Serial.println("Time out error, \t");
+          break;
+      default:
+          Serial.println("Unknown error, \t");
+          break;
+      }*/
+  // Выводим показания влажности и температуры
+  Serial.print("Vlashnost' = ");
+  Serial.print(DHT_array[0].humidity, 1);
+  Serial.print(", Temperatyra = ");
+  Serial.println(DHT_array[0].temperature,1);
+  
   
   
 
 }
 
-void loop() {
 
+void download_config()
+{
   
   
   
-
+  
+  
+  
 }
+
